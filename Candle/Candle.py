b0ce78a4cfa0115d374b32e7aba279170b3f33c1
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-04-12 17:32:30 +0900

    Creating a README file
    Creating a README file
    Modify the code

--- Candle/Candle.py
+++ Candle/Candle.py
@@ -9,8 +9,9 @@ class Candle(object):
     __ratio = {}
     __led_x = {}
     __led_k = {}
+    __led_w = {}
 
-    def __init__(self, gpioMode=GPIO.BCM):
+    def __init__(self, gpiomode=GPIO.BCM):
         super(Candle, self).__init__()
         import RPi.GPIO as GPIO
         GPIO.setwarnings(False)
@@ -26,37 +27,39 @@ class Candle(object):
         self.__ratio[pin] = float(ratio)
         self.__led_x[pin] = 0.0
         self.__led_k[pin] = 2.2904
+        self.__led_w[pin] = 10
 
     def reset(self, pin=None):
         if pin is None:
-            for i in self.pin:
+            for i in self.__pin:
                 self.__brightness[i] = 1.0
                 self.__rate[i] = 0.005
                 self.__ratio[i] = 100.0
                 self.__led_x[i] = 0.0
                 self.__led_k[i] = 2.2904
+                self.__led_w[i] = 10
         else:
             self.__brightness[pin] = 1.0
             self.__rate[pin] = 0.005
             self.__ratio[pin] = 100.0
             self.__led_x[pin] = 0.0
             self.__led_k[pin] = 2.2904
+            self.__led_w[pin] = 10
 
     def shuffle(self, min, max, pin=None):
         from random import randrange as random
         if pin is None:
-            for i in self.pin:
+            for i in self.__pin:
                 self.__led_x[i] += self.__rate[i] * random(min, max)
         else:
             self.__led_x[pin] += self.__rate[pin] * random(min, max)
 
-    def pin(self, pin=None, pwmfreq=1000):
-        if pin is None:
-            return self.__pin
-        else:
-            import RPi.GPIO as GPIO
-            self.__pin[pin] = GPIO.PWM(pin, pwmfreq)
-            self.__pin[pin].start(0)
+    def pin(self, beforepin, afterpin, pwmfreq=1000):
+        import RPi.GPIO as GPIO
+        del self.__pin[beforepin]
+        GPIO.cleanup(beforepin)
+        self.__pin[afterpin] = GPIO.PWM(afterpin, pwmfreq)
+        self.__pin[afterpin].start(0)
 
     def brightness(self, brightness, pin=None):
         if pin is None:
@@ -85,14 +88,21 @@ class Candle(object):
         else:
             self.__led_k[pin] = float(value)
 
-    def refresh(self, pin=None, wave=10):
+    def wave(self, value, pin=None):
+        if pin is None:
+            for i in self.__pin:
+                self.__led_w[i] = int(value)
+        else:
+            self.__led_w[pin] = int(value)
+
+    def refresh(self, pin=None):
         from math import sin
         led_y = 0.0
         led_y_r = []
         if pin is None:
             for j in self.__pin:
                 led_y = 0.0
-                for i in range(wave):
+                for i in range(self.__led_w[j]):
                     led_y += sin(self.__led_x[j] * self.__led_k[j] ** i) * self.__led_k[j] ** -i
                 self.__led_x[j] += self.__rate[j]
                 led_y += 1.75 + (100.0 - self.__ratio[j]) / (self.__ratio[j] / 2.0)
@@ -105,7 +115,7 @@ class Candle(object):
                 led_y_r.append(led_y)
                 self.__pin[j].ChangeDutyCycle(led_y)
         else:
-            for i in range(wave):
+            for i in range(self.__led_w[pin]):
                 led_y += sin(self.__led_x[pin] * self.__led_k[pin] ** i) * self.__led_k[pin] ** -i
             self.__led_x[pin] += self.__rate[pin]
             led_y += 1.75 + (100.0 - self.__ratio[pin]) / (self.__ratio[pin] / 2.0)
---
+++ README.md
@@ -0,0 +1,145 @@
+LED-Candle-for-Raspberry-Pi
+====
+
+## Overview
+
+LED candle library for Raspberry Pi. Using this library, you can create LED candles. Combine multiple frequencies to simulate candle fluctuations.
+With this library, you can change brightness, speed, fluctuation and more. This can be used for illumination as well as candles.
+
+## Demo
+
+````python:example.py
+from Candle import Candle
+import time
+import RPi.GPIO as GPIO
+
+sleep_ms = lambda ms: time.sleep(ms / 1000.0)
+
+candle = Candle()
+candle.create(24)
+candle.shuffle(0, 1000)
+
+for times in range(2000):
+    candle.refresh()
+    sleep_ms(10)
+
+GPIO.cleanup()
+````
+
+## Usage
+
+#### Method
+
+````python:example.py
+Candle(gpiomode=GPIO.BCM)
+````
+
+    gpiomode: How to specify GPIO number
+
+Create an instance.
+
+````python:example.py
+create(pin, brightness=100, rate=100, ratio=100, pwmfreq=1000)
+````
+
+    pin: GPIO pin number
+    brightness: Brightness level(optional)
+    rate: Speed ​​of change(optional)
+    ratio: The magnitude of the fluctuation(optional)
+    pwmfreq: PWM frequency(optional)
+
+Creates a candle on the specified pin.
+
+````python:example.py
+reset(pin=None)
+````
+
+    pin: GPIO pin number(optional)
+
+Resets the settings of the specified pin.
+If the argument is omitted, it is executed on all pins.
+
+````python:example.py
+shuffle(min, max, pin=None)
+````
+
+    min: Lower bound of shuffle range
+    max: Upper limit of shuffle range
+    pin: GPIO pin number(optional)
+
+Shuffles the start of the waveform to the specified pin between min and max.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+pin(beforepin, afterpin, pwmfreq=1000)
+````
+
+    beforepin: Pin before change
+    afterpin: Pin after change
+    pwmfreq: PWM frequency(optional)
+
+Change the output pin.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+brightness(brightness, pin=None)
+````
+
+    brightness: Brightness level
+    pin: GPIO pin number(optional)
+
+Change the brightness.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+rate(rate, pin=None)
+````
+
+    rate: Speed ​​of change
+    pin: GPIO pin number(optional)
+
+Change the speed of change.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+ratio(ratio, pin=None)
+````
+
+    ratio: The magnitude of the fluctuation
+    pin: GPIO pin number(optional)
+
+Change the size of the fluctuation.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+parameter(value, pin=None)
+````
+
+    value: Parameter value
+    pin: GPIO pin number(optional)
+
+Change the value of a parameter.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+wave(value, pin=None)
+````
+
+    value: Number of sine waves to combine
+    pin: GPIO pin number(optional)
+
+Change the number of sine waves to combine.
+If pin is omitted, execute on all pins.
+
+````python:example.py
+refresh(pin=None)
+````
+
+    pin: GPIO pin number(optional)
+
+PWM output to the specified pin.
+If the argument is omitted, it is executed on all pins.
+
+## License
+
+MIT
